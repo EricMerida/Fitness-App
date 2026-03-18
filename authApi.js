fa73ca6cca3d9f98fb7296b03ec05d994301daa1
Eric Merida <emerida623@gmail.com>
2026-03-18 14:42:03 -0400

    Delete authApi.js

--- authApi.js
+++
@@ -1,9 +0,0 @@
-import { apiFetch } from "./http";
-
-export function loginApi(credentials) {
-  return apiFetch("/auth/login", { method: "POST", body: credentials });
-}
-
-export function registerApi(payload) {
-  return apiFetch("/auth/register", { method: "POST", body: payload });
-}
