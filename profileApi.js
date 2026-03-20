0e4d4a82355c1ffc59945e8835d7d047b79d32d5
Eric Merida <emerida623@gmail.com>
2026-03-20 11:53:01 -0400

    Delete profileApi.js

--- profileApi.js
+++
@@ -1,9 +0,0 @@
-import { apiFetch } from "./http";
-
-export function getProfileApi(token) {
-  return apiFetch("/profile", { token });
-}
-
-export function updateProfileApi(token, payload) {
-  return apiFetch("/profile", { method: "PUT", body: payload, token });
-}
