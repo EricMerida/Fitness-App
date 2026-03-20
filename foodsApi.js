ab6451f4c73f9b60296631f04c93ff35115603df
Eric Merida <emerida623@gmail.com>
2026-03-20 11:52:20 -0400

    Delete foodsApi.js

--- foodsApi.js
+++
@@ -1,5 +0,0 @@
-import { apiFetch } from "./http";
-
-export function searchFoodsApi(token, q) {
-  return apiFetch(`/foods/search?q=${encodeURIComponent(q)}&pageSize=10&pageNumber=1`, { token });
-}
